c8cdd04a375e5ae6f21167a17e1ee017b8b04f90
Maor Cohen <maor@Shaloms-MacBook-Air.local>
2019-04-18 21:13:43 -0400

    updated model and requirements

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # sqft_living_vs._date
-Analyzing the relationship of the sqft of living vs. the sold price using regression.
+Predicting the price of a house from the sqft of living using regression
--- code_files/model.py
+++ code_files/model.py
@@ -4,10 +4,12 @@ from sklearn import preprocessing, model_selection
 import pandas as pd
 import numpy as np
 
-#loading the data and extracting the valueable info
+#loading the data
 df = pd.read_csv("/Users/maor/Documents/src/house_prices_vs._date/date_vs_price.csv")
-sqft = np.array(df[['sqft_living']], dtype = np.float64)
-prices = np.array(df[['price']], dtype = np.float64)
 
-plt.scatter(prices, sqft)
-plt.show()
+#creating the xs and ys
+xs = df[['sqft_living']]
+ys = df[['price']]
+
+#creaing the training and testing data for the model
+x_train, x_test, y_train, y_test = model_selection.train_test_split(xs, ys, test_split = 0.3)
